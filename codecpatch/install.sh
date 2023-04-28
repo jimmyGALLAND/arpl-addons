47fdbcea38dd4c19ca0e6b1b9faf19367ea6e92e
Ing <wjz304@qq.com>
2023-04-29 05:28:08 +0800

    mod service ln

--- codecpatch/install.sh
+++ codecpatch/install.sh
@@ -4,6 +4,7 @@ if [ "${1}" = "late" ]; then
   echo "Installing addon synocodec patch"
   cp -v /usr/bin/codecpatch.sh /tmpRoot/usr/bin/codecpatch.sh
   cp -v /usr/lib/systemd/system/codecpatch.service /tmpRoot/usr/lib/systemd/system/codecpatch.service
-  mkdir -vp /tmpRoot/lib/systemd/system/multi-user.target.wants
-  ln -vsf /usr/lib/systemd/system/codecpatch.service /tmpRoot/lib/systemd/system/multi-user.target.wants/codecpatch.service
+  # /lib -> /usr/lib; /etc/systemd/system/multi-user.target.wants/* -> /usr/lib/systemd/system/multi-user.target.wants/*
+  mkdir -vp /tmpRoot/usr/lib/systemd/system/multi-user.target.wants
+  ln -vsf /usr/lib/systemd/system/codecpatch.service /tmpRoot/usr/lib/systemd/system/multi-user.target.wants/codecpatch.service
 fi
--- cpuinfo/install.sh
+++ cpuinfo/install.sh
@@ -4,6 +4,7 @@ if [ "${1}" = "late" ]; then
   echo "Installing daemon for CPU Info"
   cp -v /usr/bin/cpuinfo.sh /tmpRoot/usr/bin/cpuinfo.sh
   cp -v /usr/lib/systemd/system/cpuinfo.service /tmpRoot/usr/lib/systemd/system/cpuinfo.service
-  mkdir -vp /tmpRoot/lib/systemd/system/multi-user.target.wants
-  ln -vsf /usr/lib/systemd/system/cpuinfo.service /tmpRoot/lib/systemd/system/multi-user.target.wants/cpuinfo.service
+  # /lib -> /usr/lib; /etc/systemd/system/multi-user.target.wants/* -> /usr/lib/systemd/system/multi-user.target.wants/*
+  mkdir -vp /tmpRoot/usr/lib/systemd/system/multi-user.target.wants
+  ln -vsf /usr/lib/systemd/system/cpuinfo.service /tmpRoot/usr/lib/systemd/system/multi-user.target.wants/cpuinfo.service
 fi
